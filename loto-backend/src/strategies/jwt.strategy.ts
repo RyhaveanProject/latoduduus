91449f20102b7ff99071e316372925896d28cd71
RyhaveanProject <bfnsscxj97@privaterelay.appleid.com>
2026-06-22 00:12:57 +0400

    Add files via upload

---
+++ loto-backend/src/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayloadDto } from '../dtos/auth.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET', 'secret'),
+    });
+  }
+
+  validate(payload: any): TokenPayloadDto {
+    return {
+      sub: payload.sub,
+      email: payload.email,
+      role: payload.role,
+      iat: payload.iat,
+      exp: payload.exp,
+    };
+  }
+}
